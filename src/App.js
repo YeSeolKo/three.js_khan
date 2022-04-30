9c627fa635e2522cfea775245da8e6f178003026
yeseol <39734418+YeSeolKo@users.noreply.github.com>
2022-04-30 17:01:31 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,35 @@
-import { useEffect, useState, useRef } from "react";
 import "./styles.css";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 
 export default function App() {
-  function Box(props) {
-    const ref = useRef();
-
-    const [hovered, sethover] = useState(false);
-
-    useEffect(() => {
-      console.log(hovered);
-    }, [hovered]);
-
-    useFrame(() => {
-      ref.current.rotation.x += 0.01;
-    });
-
-    return (
-      <mesh
-        onPointerOver={(e) => sethover(true)}
-        onPointerOut={(e) => sethover(false)}
-        ref={ref}
-        {...props}
-        scale={hovered ? 2 : 1.5}
-      >
-        <boxGeometry args={[1, 1, 3]} />
-        <meshStandardMaterial color={hovered ? "orange" : "red"} />
-      </mesh>
-    );
-  }
-
   return (
     <div className="App">
-      <Canvas style={{ height: "100vh" }}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
+      <nav class="nav">
+        <a class="nav-link active" aria-current="page" href="#">
+          Rolex
+        </a>
+        <a class="nav-link" href="#">
+          Link
+        </a>
+        <a class="nav-link" href="#">
+          Link
+        </a>
+        <a class="nav-link disabled">Disabled</a>
+      </nav>
+
+      <div className="bg-new">
+        <div className="row">
+          <div className="col-lg-5 col-md-5 col-sm-12">
+            <h3 class="display-4 text-center">
+              Check out our best and the cheapest watches{" "}
+            </h3>
+          </div>
+
+          <div className="col-lg-6 col-md-5 col-sm-12">
+            <Canvas></Canvas>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
